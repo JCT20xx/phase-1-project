9d14887690cc0a3fcfe24599e6fdc5e24d3095b5
Juan Toro <jct20xx@gmail.com>
2022-05-15 23:53:15 -0400

    Finished adding the last required event listners and the forEach i needed to iterate through types and moves

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ Going to change to a differnt API as the one i chose seem disorganized for my pu
 Changed to use a pokemon API and it is a much cleaner api to work with
 Added div to index.html to add in the pokemon info
 Added the event listener to clear the page and a back button to reload list
+Rearranged alot of the const for the tags but its working now
+there is a forEach to get the pokemons typing and a forEach to get all the moves
--- index.js
+++ index.js
@@ -6,21 +6,20 @@ document.addEventListener('DOMContentLoaded', () => {
 let pokemon
 
 function loadPokemon(){
-    const div = document.querySelector('div')
-    div.innerHTML = ''
+    const div = document.querySelectorAll('div')
+    div.forEach(div => div.innerText = '')
     fetch(site)
     .then(resp => resp.json())
     .then(data => {
-        console.log(data)
         pokemon = data
         listPokemon()
     })}
 
 function listPokemon(){
-    const ul = document.getElementById('pokemon_list')
+    const ol = document.querySelector('ol')
     pokemon.results.forEach(mon => {
         const li = document.createElement('li')
-        ul.appendChild(li)
+        ol.appendChild(li)
         const a = document.createElement('a')
         a.innerText = mon.name
         a.id = mon.url
@@ -28,24 +27,61 @@ function listPokemon(){
         a.href='#'
         li.appendChild(a)
     })
-    ul.addEventListener('click', whoseThatPokemon)
+    ol.addEventListener('click', whoseThatPokemon)
 }
 
 function whoseThatPokemon(e){
-    const pokemon = e.target.innerText
-    const ul = document.getElementById('pokemon_list')
-    ul.innerHTML = ''
+    const pokemon = e.target.id
+    const ol = document.querySelector('ol')
+    if (pokemon != ''){
+    ol.innerHTML = ''
     fetch(e.target.id)
     .then(resp => resp.json())
-    .then(data => console.log(data))
-    const div = document.querySelector('div')
+    .then(data => {
     
-    
-    
-    
+    const div_info = document.getElementById('info')
+    const div_movelist = document.getElementById('movelist')
+    const div_stats = document.getElementById('stats')
+    const h1 = document.createElement('h1')
+    const img = document.createElement('img')
     const back = document.createElement('form')
+    
+    h1.id = "NAME"
+    h1.innerText = `${data.name} #${data.id}`
+    img.src = data.sprites.front_default
+    img.alt = data.name
     back.innerHTML = '<form><input type="button" value="Main List"></form>'
-    div.appendChild(back)  
+
+    div_info.appendChild(h1)
+    div_info.appendChild(back) 
+    div_info.appendChild(img)
+
+    div_stats.innerHTML = '<div id="stats"> <ul id="type"> <li></li> </ul>    <ul id="hw"> <li id="height"></li> <li id= "weight"></li> </ul>  </div>     <div id="movelist"></div>'
+   
+    const ul_types = document.getElementById('type')
+    const ul_hw = document.getElementById('hw')
+    const height = document.getElementById('height')
+    const weight = document.getElementById('weight')
+    height.innerText= `Height: ${data.height} DeciMeters`
+    weight.innerText= `Weight: ${data.weight} HectoGrams`
+
+    data.types.forEach(type => {
+    const li = document.createElement('li')
+    li.id = 'slot'
+    li.innerText = `Type:${type.type.name}`
+    ul_types.appendChild(li)
+})
+
+div_movelist.innerHTML= '<div id="movelist"> <ul id=moves></ul></div>'
+const ul_moves = document.getElementById('moves')
+data.moves.forEach(move => {
+    const li = document.createElement('li')
+    li.id = 'move'
+    li.innerText = `Move: ${move.move.name}`
+    ul_moves.appendChild(li)
+})
+   
+
     back.addEventListener('click', loadPokemon)
-}
+})}}
 
